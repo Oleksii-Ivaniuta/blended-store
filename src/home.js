858746374f294acd18fe72ccf50838730c9ce9e0
19taurus79 <19taurus79@gmail.com>
2025-04-12 17:16:08 +0300

    add delegation category

--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
-import { onDOMContentLoaded } from './js/handlers';
+import { onCategoryClick, onDOMContentLoaded } from './js/handlers';
 
 //Логіка сторінки Home
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+
+document.addEventListener('click', (onCategoryClick));
--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,4 +1,4 @@
-import { getCategories, getProducts } from './products-api';
+import { getCategories, getProducts, getProductsByCategory } from './products-api';
 import { renderCategories, renderProducts } from './render-function';
 
 // Функції, які передаються колбеками в addEventListners
@@ -8,6 +8,40 @@ export async function onDOMContentLoaded() {
     renderCategories(categories);
 
     const { products } = await getProducts();
+    renderProducts(products);
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function onCategoryClick(event) {
+  try {
+    if (event.target.tagName !== 'BUTTON') {
+      return;   
+    }
+    const category = event.target.textContent;
+    const categoryBtn = event.target
+    const notFound = document.querySelector('.not-found');
+    console.log(category)
+    if (category === 'All') {
+      const { products } = await getProducts();
+      console.log('all',products);
+      
+      renderProducts(products);
+      return;
+    }
+    const { products } = await getProductsByCategory(category);
+    console.log(products.length);
+    
+    if (products.length === 0) {
+      console.log('notFound');
+      notFound.classList.add('not-found--visible');
+      return;
+     };
+    categoryBtn.classList.add('categories__btn--active');
+    //TODO: remove
+    console.log(products);
+    console.log(categoryBtn);
+    
     renderProducts(products);
   } catch (error) {
     console.log(error);
