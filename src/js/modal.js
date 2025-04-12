acfaf566118e781328657d4ec01f9d643c798913
IhorMtr <igormotornyyy@gmail.com>
2025-04-12 15:59:30 +0300

    changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,27 @@
 //Описана робота модалки - відкриття закриття і все що з модалкою повʼязано
+const modal = document.querySelector('.modal');
+
+export function openModal() {
+  modal.classList.add('modal--is-open');
+
+  document.addEventListener('keydown', handleEscapeClose);
+  modal.addEventListener('click', handleBackdropClickClose);
+}
+
+export function closeModal() {
+  modal.classList.remove('modal--is-open');
+  removeEventListeners();
+}
+
+function removeEventListeners() {
+  document.removeEventListener('keydown', handleEscapeClose);
+  modal.removeEventListener('click', handleBackdropClickClose);
+}
+
+function handleEscapeClose(event) {
+  if (event.key === 'Escape') closeModal();
+}
+
+function handleBackdropClickClose(event) {
+  if (event.target === modal) closeModal();
+}
