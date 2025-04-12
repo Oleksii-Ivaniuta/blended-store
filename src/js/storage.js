1eaeac8ef904c7d6a1d3c86a0be3b2b2ca27dd85
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-04-12 13:39:21 +0100

    add storage features

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,1 +1,18 @@
 //Робота з loacalStorage
+export function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function loadFromLocalStorage(key) {
+    try {
+        const savedData = JSON.parse(localStorage.getItem(key));
+    return savedData; 
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export function removeFromLocalStorage(key) {
+    localStorage.removeItem(key);
+}
