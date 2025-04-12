0fb04911bfa6de7b58a8248a953ace5520177cf0
19taurus79 <19taurus79@gmail.com>
2025-04-12 15:55:54 +0300

    create functions api_products

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,11 +1,27 @@
 // Функції для роботи з бекендом
 import axios from 'axios';
+axios.defaults.baseURL = 'https://dummyjson.com/products';
 export async function getCategories() {
-    const categories = await axios.get('https://dummyjson.com/products/categories')
-    return categories.data;
+    const {data} = await axios.get('/categories')
+    return data;
 }
 
-export async function getProductsFromCategory(category) {
-    const products = await axios.get(`https://dummyjson.com/products/category/${category}`)
-    return products.data.products;
- };
+export async function getProductsByCategory(category, page=1) {
+    const {data} = await axios.get(`/category/${category}?limit=12&skip=${(page - 1) * 12}`)
+    return data;
+};
+ 
+export async function getProducts(page=1) {
+    const {data} = await axios.get(`?limit=12&skip=${(page - 1) * 12}`)
+    return data;
+};
+
+export async function getProductById(id) {
+    const {data} = await axios.get(`/${id}`)
+    return data;
+};
+
+export async function getProductsByQuery(query, page=1) {
+  const {data} = await axios.get(`/search?q=${query}&limit=12&skip=${(page - 1) * 12}`)
+  return data;
+};
